4b602c05753033a46b971581e51956f61d3b6ac2
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-05-21 16:45:06 -0700

    testing git

--- regex.js
+++ regex.js
@@ -1,17 +1,17 @@
 module.exports = {
-   getComponents(file) {
-      return "use the string .match() method and regex to find what you are looking for"
-   },
-   getName(component) {
-      return "use the string .match() method and regex to find what you are looking for"
-   },
-   getDesc(component) {
-      return "use the string .match() method and regex to find what you are looking for"
-   },
-   getInputs(component) {
-      return "use the string .match() method and regex to find what you are looking for"
-   },
-   trim(str) {
-      return "use the string .replace() method and regex to first replace carriage returns and new lines with a space, then replace 2 or more spaces with 1 space, then remove spaces from beginning and end"
-   },
+  getComponents(file) {
+    return "use the string .match() method and regex to find what you are looking for";
+  },
+  getName(component) {
+    return "use the string .match() method and regex to find what you are looking for";
+  },
+  getDesc(component) {
+    return "use the string .match() method and regex to find what you are looking for";
+  },
+  getInputs(component) {
+    return "use the string .match() method and regex to find what you are looking for";
+  },
+  trim(str) {
+    return "use the string .replace() method and regex to first replace carriage returns and new lines with a space, then replace 2 or more spaces with 1 space, then remove spaces from beginning and end";
+  },
 };
